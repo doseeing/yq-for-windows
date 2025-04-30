9fd6e11d545f6dfe8a61b421acfde899fcc3c9fe
doseeing <doseeing@gmail.com>
2025-04-30 10:20:36 +0800

    Update IPCs.ts

--- src/main/IPCs.ts
+++ src/main/IPCs.ts
@@ -1,15 +1,37 @@
-import { ipcMain, shell, IpcMainEvent, dialog } from 'electron'
+import { ipcMain, IpcMainEvent, dialog } from 'electron'
 import Constants from './utils/Constants'
 import { execFile } from 'child_process'
 import path from 'path'
 import { promisify } from 'util'
 import stream from 'stream'
 
+function chooseBinaryByPlatform(): string {
+  const platform = process.platform
+  const arch = process.arch
+  let binaryName = ''
+  if (platform === 'win32') {
+    binaryName = 'yq_windows_amd64.exe'
+  }
+  if (platform === 'linux') {
+    binaryName = 'yq_linux_amd64'
+  }
+  if (platform === 'darwin') {
+    // check arch
+    if (arch === 'arm64') {
+      binaryName = 'yq_darwin_arm64'
+    } else {
+      binaryName = 'yq_darwin_amd64'
+    }
+  }
+  return binaryName
+}
+
 function runBinary(input: string, callback: (error: Error | null, result: string) => void): void {
+  const binaryName = chooseBinaryByPlatform()
   const binaryPath =
     process.env.NODE_ENV === 'development'
-      ? path.join(__dirname, '..', '..', 'bin', 'yq_darwin_arm64')
-      : path.join(process.resourcesPath, 'bin', 'yq_darwin_arm64')
+      ? path.join(__dirname, '..', '..', 'bin', binaryName)
+      : path.join(process.resourcesPath, 'bin', binaryName)
   console.log('Running binary at:', binaryPath)
 
   const child = execFile(binaryPath, ['-P'], (error, stdout) => {
